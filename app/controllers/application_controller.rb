d85b571a50ff3ba7fa0b8d156f8e1aa491360045
airzt11 <airzonatea@gmail.com>
2021-04-26 16:22:16 -0400

    updated secret for JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
   def encode_token(payload)
     # should store secret in env variable
-    JWT.encode(payload, ENV['MY_SECRET'], 'HS256')
+    JWT.encode(payload, 'secret', 'HS256')
   end
  
   def auth_header
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::API
   end
  
   def decode(token)
-    JWT.decode(token, ENV['MY_SECRET'], true, {algorithm: 'HS256'})[0]
+    JWT.decode(token, 'secret', true, {algorithm: 'HS256'})[0]
   end
 
 end
