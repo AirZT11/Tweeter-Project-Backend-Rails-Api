a5a855f628012a9e9704093063f45fa423dd9506
airzt11 <airzonatea@gmail.com>
2021-05-02 20:48:45 -0400

    changed cors origin to Heroku app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://tweeter-heroku-frontend.herokuapp.com'
+    origins 'http://tweeter-heroku-frontend.herokuapp.com/'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
 end
